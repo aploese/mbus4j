edf830902190aeb458bd26bd594eb0d92bdd1181
arnep <arnep@a79c86be-501a-4e4d-859e-876426ae6b50>
2011-09-04 08:53:04 +0000

    failed release

--- tags/mbus4j-0.1.4/run.site.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export MAVEN_OPTS="-Xmx2028M -XX:MaxPermSize=256M" &&
-mvn -e site
--- tags/mbus4j-0.1.4/trunk/subversion_tools.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-find ./ -name "*.java" -exec svn propset svn:keywords "Id"  {} \;
-find ./ -name "*.betwixt" -exec svn propset svn:keywords "Id"  {} \;
-
-echo Files without \$Id\:
-find ./ -name "*.java" -exec grep -L \$Id {} \;
-find ./ -name "*.betwixt" -exec grep -L \$Id {} \;
-
