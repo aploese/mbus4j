dfc4fa28058ef3ae422296467358a6af9757526e
arnep <aploese@users.noreply.github.com>
2014-02-21 09:34:22 +0000

    added missing stuff ;-)

---
+++ mbus4j-master-ui/src/test/java/net/sf/mbus4j/DummyAspect.java
@@ -0,0 +1,56 @@
+package net.sf.mbus4j;
+
+/*
+ * #%L
+ * mbus4j-master-ui
+ * %%
+ * Copyright (C) 2009 - 2014 MBus4J
+ * %%
+ * mbus4j - Drivers for the M-Bus protocol - http://mbus4j.sourceforge.net/
+ * Copyright (C) 2009-2014, mbus4j.sf.net, and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ * 
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * #L%
+ */
+
+import java.util.logging.Logger;
+import net.sf.mbus4j.log.LogUtils;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
+/**
+ *
+ * @author aploese
+ */
+@Aspect
+public class DummyAspect {
+
+    private static Logger LOG = LogUtils.getMasterLogger();
+
+    @Pointcut("execution(* Dummy.blubber(..))")
+    public void blubber() {
+    }
+
+    @Before("blubber()")
+    public void say(JoinPoint jp) {
+        LOG.fine("Vorher AAAA");
+    }
+
+}
