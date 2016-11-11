1cee0129b3f070739fda1ff9e18875970415e7d8
arnep <arnep@a79c86be-501a-4e4d-859e-876426ae6b50>
2016-11-11 19:49:50 +0000

    [maven-release-plugin] copy for tag Release_mbus4j_1.0.0

---
+++ tags/Release_mbus4j_1.0.0/mbus4j-core/src/main/java/net/sf/mbus4j/dataframes/GeneralApplicationError.java
@@ -0,0 +1,242 @@
+package net.sf.mbus4j.dataframes;
+
+/*
+ * #%L
+ * mbus4j-core
+ * %%
+ * Copyright (C) 2009 - 2014 MBus4J
+ * %%
+ * mbus4j - Drivers for the M-Bus protocol - http://mbus4j.sourceforge.net/
+ * Copyright (C) 2009-2014, mbus4j.sf.net, and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ * 
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * #L%
+ */
+import java.util.Iterator;
+import net.sf.json.JSONObject;
+
+import net.sf.mbus4j.dataframes.datablocks.DataBlock;
+import net.sf.mbus4j.json.JsonSerializeType;
+
+/**
+ *
+ * @author arnep@users.sourceforge.net
+ * @version $Id$
+ */
+public class GeneralApplicationError implements LongFrame, PrimaryAddress {
+
+    public static String RSP_UD_SUBTYPE = "general application error";
+
+    @Override
+    public JSONObject toJSON(JsonSerializeType jsonSerializeType) {
+        JSONObject result = new JSONObject();
+        result.accumulate("controlCode", getControlCode());
+        result.accumulate("subType", RSP_UD_SUBTYPE);
+        if (jsonSerializeType.ALL == jsonSerializeType) {
+            result.accumulate("acd", isAcd());
+            result.accumulate("dfc", isDfc());
+            result.accumulate("address", address & 0xFF);
+            result.accumulate("error", error.getLabel());
+        }
+        return result;
+    }
+
+    @Override
+    public void fromJSON(JSONObject json) {
+        acd = json.getBoolean("acd");
+        dfc = json.getBoolean("dfc");
+        address = (byte) json.getInt("address");
+        error = GeneralApplicationErrorEnum.fromLabel(json.getString("error"));
+    }
+
+    public static enum GeneralApplicationErrorEnum {
+
+        UNSPECIFIED_ERROR(0x00, "Unspecified error"),
+        UNIMPLEMENTED_CI_FIELD(0x01, "unimplemented ci-field"),
+        BUFFER_TOO_LONG(0x02, "buffer too long, truncated"),
+        TOO_MANX_RECORDS(0x03, "too manx records"),
+        REMATURE_END_OF_RECORDS(0x04, "premature end of records"),
+        MORE_THAN_10_DIFES(0x05, "more tham 10 DIFE's"),
+        MORE_THAN_10_VIFES(0x06, "more tham 10 VIFE's"),
+        RESERVED_0X07(0x07, "Reserved 0x07"),
+        APP_TOO_BUSY_FOR_HANDLING_READOUT_REQUEST(0x08, "application to busy for handling readout request"),
+        TOO_MANY_READOUTS(0x09, "too many readouts(for slaves with limited readouts per time");
+
+        private static GeneralApplicationErrorEnum valueOf(int id) {
+            for (GeneralApplicationErrorEnum e : GeneralApplicationErrorEnum.values()) {
+                if (e.id == id) {
+                    return e;
+                }
+            }
+            return null;
+        }
+        public final byte id;
+        private final String label;
+
+        private GeneralApplicationErrorEnum(int id, String label) {
+            this.id = (byte) id;
+            this.label = label;
+        }
+
+        @Override
+        public String toString() {
+            return label;
+        }
+
+        public String getLabel() {
+            return label;
+        }
+
+        public static GeneralApplicationErrorEnum fromLabel(String label) {
+            for (GeneralApplicationErrorEnum value : values()) {
+                if (value.label.equals(label)) {
+                    return value;
+                }
+            }
+            return valueOf(label);
+        }
+    }
+    private boolean acd;
+    private boolean dfc;
+    private byte address;
+    private GeneralApplicationErrorEnum error;
+
+    public GeneralApplicationError() {
+        super();
+    }
+
+    public GeneralApplicationError(UserDataResponse old) {
+        this.acd = old.isAcd();
+        this.dfc = old.isDfc();
+        this.address = old.getAddress();
+    }
+
+    @Override
+    public boolean addDataBlock(DataBlock dataBlock) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    /**
+     * @return the address
+     */
+    @Override
+    public byte getAddress() {
+        return address;
+    }
+
+    @Override
+    public ControlCode getControlCode() {
+        return ControlCode.RSP_UD;
+    }
+
+    /**
+     * @return the error
+     */
+    public GeneralApplicationErrorEnum getError() {
+        return error;
+    }
+
+    @Override
+    public DataBlock getLastDataBlock() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    /**
+     * @return the acd
+     */
+    public boolean isAcd() {
+        return acd;
+    }
+
+    /**
+     * @return the dfc
+     */
+    public boolean isDfc() {
+        return dfc;
+    }
+
+    @Override
+    public Iterator<DataBlock> iterator() {
+        return new Iterator<DataBlock>() {
+
+            @Override
+            public boolean hasNext() {
+                return false;
+            }
+
+            @Override
+            public DataBlock next() {
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
+        };
+    }
+
+    @Override
+    public void replaceDataBlock(DataBlock oldDataBlock, DataBlock newDataBlock) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    /**
+     * @param acd the acd to set
+     */
+    public void setAcd(boolean acd) {
+        this.acd = acd;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    @Override
+    public void setAddress(byte address) {
+        this.address = address;
+    }
+
+    /**
+     * @param dcf the dfc to set
+     */
+    public void setDfc(boolean dfc) {
+        this.dfc = dfc;
+    }
+
+    public void setError(byte b) {
+        error = GeneralApplicationErrorEnum.valueOf(b);
+    }
+
+    /**
+     * @param error the error to set
+     */
+    public void setError(GeneralApplicationErrorEnum error) {
+        this.error = error;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("control code = ").append(getControlCode()).append('\n');
+        sb.append("isAcd = ").append(isAcd()).append('\n');
+        sb.append("isDfc = ").append(isDfc()).append('\n');
+        sb.append(String.format("address = 0x%02X\n", address));
+        sb.append("error = ").append(error).append('\n');
+        return sb.toString();
+    }
+}
---
+++ tags/Release_mbus4j_1.0.0/mbus4j-core/src/test/java/net/sf/mbus4j/encoder/UserDataResponseTest.java
@@ -0,0 +1,211 @@
+package net.sf.mbus4j.encoder;
+
+/*
+ * #%L
+ * mbus4j-core
+ * %%
+ * Copyright (C) 2009 - 2014 MBus4J
+ * %%
+ * mbus4j - Drivers for the M-Bus protocol - http://mbus4j.sourceforge.net/
+ * Copyright (C) 2009-2014, mbus4j.sf.net, and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ * 
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * #L%
+ */
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import net.sf.mbus4j.dataframes.Frame;
+
+import net.sf.mbus4j.dataframes.MBusMedium;
+import net.sf.mbus4j.decoder.Decoder;
+import net.sf.mbus4j.decoder.DecoderListener;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author arnep@users.sourceforge.net
+ * @version $Id$
+ */
+public class UserDataResponseTest implements DecoderListener {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    private Decoder parser;
+    private Encoder instance;
+    private List<Frame> frames;
+
+    public UserDataResponseTest() {
+    }
+
+    @Before
+    public void setUp() {
+        frames = new ArrayList<>();
+        parser = new Decoder(this);
+        instance = new Encoder();
+    }
+
+    @After
+    public void tearDown() {
+        instance = null;
+        parser = null;
+    }
+
+    @Test
+    public void testABB_HEAT_8_3476_0() throws Exception {
+        testPackage("ABB", MBusMedium.HEAT, 8, 3476, 0);
+    }
+
+    @Test
+    public void testACW_HEAT_11_8772050_0() throws Exception {
+        testPackage("ACW", MBusMedium.HEAT, 11, 8772050, 0);
+    }
+
+    @Test
+    public void testACW_HEAT_9_6522360_0() throws Exception {
+        testPackage("ACW", MBusMedium.HEAT, 9, 6522360, 0);
+    }
+
+    @Test
+    public void testEMH_ELECTRICITY_9_332092_0() throws Exception {
+        testPackage("EMH", MBusMedium.ELECTRICITY, 9, 332092, 0);
+    }
+
+    @Test
+    public void testEMU_32_5_701841_0() throws Exception {
+        testPackage("EMU", MBusMedium.RESERVED_0X20, 5, 701841, 0);
+    }
+
+    @Test
+    public void testLUG_HEAT_2_65068549_0() throws Exception {
+        testPackage("LUG", MBusMedium.HEAT, 2, 65068549, 0);
+    }
+
+    private void testPackage(String manufacturerId, MBusMedium medium,
+            int version, int identNumber, int packetIndex) throws Exception {
+        testPackage(manufacturerId, String.format("%s-%s-%d-%d-%d", manufacturerId, medium.name(), version, identNumber, packetIndex));
+    }
+
+    private void testPackage(String manufacturerId, MBusMedium medium,
+            int version, int identNumber, int packetIndex, String comment) throws Exception {
+        testPackage(manufacturerId, String.format("%s-%s-%d-%d-%d-%s", manufacturerId, medium.name(), version, identNumber, packetIndex, comment));
+    }
+
+    private void testPackage(final String man, final String deviceName) throws IOException {
+        System.out.println("testPackage: " + deviceName);
+        InputStream is = UserDataResponseTest.class.getResourceAsStream(String.format("../byMAN/%s/%s.txt", man, deviceName));
+        BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+
+        final byte[] data = Decoder.ascii2Bytes(br.readLine());
+        for (byte b : data) {
+            parser.addByte(b);
+        }
+        assertEquals("ParserState", Decoder.DecodeState.EXPECT_START, parser.getState());
+        assertEquals("DataValue not available", 1, frames.size());
+        byte[] result = instance.encode(frames.get(0));
+        assertArrayEquals(data, result);
+    }
+
+    @Test
+    public void testPAD_WATER_1_12345678_0() throws Exception {
+        testPackage("PAD", MBusMedium.WATER, 1, 12345678, 0);
+    }
+
+    @Test
+    public void testREL_GAS_8_3043269_0() throws Exception {
+        testPackage("REL", MBusMedium.GAS, 8, 3043269, 0);
+    }
+
+    @Test
+    public void testREL_GAS_9_5119949_0() throws Exception {
+        testPackage("REL", MBusMedium.GAS, 9, 5119949, 0);
+    }
+
+    @Test
+    public void testREL_GAS_9_8077237_0() throws Exception {
+        testPackage("REL", MBusMedium.GAS, 9, 8077237, 0);
+    }
+
+    @Test
+    public void testREL_HEAT_COST_ALLOCATOR_64_13131313_0() throws Exception {
+        testPackage("REL", MBusMedium.HEAT_COST_ALLOCATOR, 64, 13131313, 0);
+    }
+
+    @Test
+    public void testSIE_HEAT_1_60109158_0() throws Exception {
+        testPackage("SIE", MBusMedium.HEAT, 1, 60109158, 0);
+    }
+
+    @Test
+    public void testSLB_WATER_2_1309125_0() throws Exception {
+        testPackage("SLB", MBusMedium.WATER, 2, 1309125, 0);
+    }
+
+    @Test
+    public void testSLB_WATER_3_99365425_0() throws Exception {
+        testPackage("SLB", MBusMedium.WATER, 3, 99365425, 0);
+    }
+
+    @Test
+    public void testSPX_HEAT_52_44350175_0_No_Vife() throws Exception {
+        testPackage("SPX", MBusMedium.HEAT, 52, 44350175, 0, "No_Vife");
+    }
+
+    @Test
+    public void testSPX_HEAT_52_54850059_0_With_Vife() throws Exception {
+        testPackage("SPX", MBusMedium.HEAT, 52, 54850059, 0, "With_Vife");
+    }
+
+    @Test
+    public void testTCH_HEAT_1_44830614_0() throws Exception {
+        testPackage("TCH", MBusMedium.HEAT, 1, 44830614, 0);
+    }
+
+    @Test
+    public void testTCH_HEAT_38_21519982_0() throws Exception {
+        testPackage("TCH", MBusMedium.HEAT, 38, 21519982, 0);
+    }
+
+    @Test
+    public void testTCH_HEAT_38_21519982_1() throws Exception {
+        testPackage("TCH", MBusMedium.HEAT, 38, 21519982, 1);
+    }
+
+    @Override
+    public void success(Frame parsingFrame) {
+        frames.add(parsingFrame);
+    }
+
+}
